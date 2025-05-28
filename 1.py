034aa762cc0484799acf44b295505fc9ce14ace7
unknown <woonnasaicharan1111@gmail.com>
2025-05-28 19:46:06 +0530

    sai charan

---
+++ 1.py
@@ -0,0 +1,12 @@
+def one_to_three() :
+    print(1, 2, 3)
+
+def three_to_seven():
+    print(3\n  4 5 6 7 )
+
+
+one_to_three
+three_to_seven
+
+
+
---
+++ def.py
@@ -0,0 +1,16 @@
+def greet(lang , name):
+
+    if lang == "en":
+
+        print("Hello")
+
+    elif lang == "fr":
+
+        print("Bonjour")
+
+    else:
+
+        print("Hola")
+
+greet("su","charan")
+ 
